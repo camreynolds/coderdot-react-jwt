e5011406418574d4845d42dacd21ccd4d0cb0f6c
carlos <camr.personal@gmail.com>
2024-12-14 17:36:35 -0500

    Dashboard frontend done.

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,8 +1,66 @@
-import React from 'react'
+import React,{useState,useEffect}from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Container,Row,Col,Table } from 'react-bootstrap'
 
 const Dashboard = () => {
+
+  const token = localStorage.getItem("token")
+  const [user, setUser] = useState()
+  const navigate = useNavigate()
+
+  useEffect(()=>{
+
+    const fetchUser = async () =>{
+      try{
+        const response = await fetch("http://localhost:5000/api/user",{
+          method: "GET",
+          headers:{
+            "Content-Type":"application/json",
+            "Authorization":`Bearer ${token}`
+          }
+        })
+  
+        const result = await response.json()
+        console.log(result)
+        setUser(result)
+  
+      }catch(error){
+        console.log(error.message);
+      }
+    }
+
+    if(token){
+      fetchUser()
+    }else{
+      navigate("/login")
+    }
+
+  },[token,navigate])
+
   return (
-    <div>Dashboard</div>
+    <Container className='mt-5'>
+      <Row>
+        <Col>
+          <h1 className='text-center'>Dashboard</h1>
+          <Table striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody>
+              {user && user.name && user.email && (
+                <tr key={user._id}>
+                  <td>{user.name}</td>
+                  <td>{user.email}</td>
+                </tr>
+              )}
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
